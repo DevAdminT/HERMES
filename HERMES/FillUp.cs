e30d66c74e395f1810dbd71b3adec5176326963f
Tyler <DevAdmint@tylerryork.com>
2018-10-17 20:11:00 -0500

    added initial location services

---
+++ HERMES/FillUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UIKit;
+using Foundation;
+using HERMES;
+using CoreLocation;
+
+namespace HERMES
+{
+    public class FillUp
+    {
+        public FillUp()
+        {
+        }
+
+        public static FillUpLocation GetCurrentLocation ()
+        {
+            CLLocationManager LocationManager = null;
+            FillUpLocation theLocation = null;
+            LocationManager.DesiredAccuracy = 1000;
+            LocationManager.UpdatedLocation += (object sender, CLLocationUpdatedEventArgs e) =>
+            {
+                theLocation = UpdateLocation(e.NewLocation);
+            };
+
+                return theLocation;
+        }
+        static public FillUpLocation UpdateLocation(CLLocation newLocation)
+        {
+            FillUpLocation theLocation = new FillUpLocation (newLocation.Coordinate.Longitude.ToString(),
+                                                            newLocation.Coordinate.Latitude.ToString(),
+                                                            newLocation.Altitude.ToString());
+
+            return theLocation;
+        }
+    }
+}
---
+++ HERMES/FillUpLocation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HERMES
+{
+    public class FillUpLocation
+    {
+        public static string Longitude { get; set; }
+        public static string Latitude { get; set; }
+        public static string Altitude { get; set; }
+
+        public FillUpLocation (string theLong, string theLat, string theAlt)
+        {
+            Longitude = theLong;
+            Latitude = theLat;
+            Altitude = theAlt;
+        }
+
+        public FillUpLocation()
+        {
+        }
+    }
+}
